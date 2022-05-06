3099f4a0694e8bc3f7ca2e66d42a0b8c02e519c7
adrian-kriegel <adrian@unolog.in>
2022-05-06 11:26:42 +0200

    using jwt for test keys

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,13 +1,11 @@
 
+import jwt from 'jsonwebtoken';
+
 /**
  * @param appId appId
  * @returns fake API-token that can be used for local testing
  */
 export function createApiToken(appId: string)
 {
-  return Buffer.from(
-    JSON.stringify(
-      {payload: {data: { appId }}},
-    ),
-  ).toString('base64');
+  return jwt.sign({ appId }, 'guest');
 }
